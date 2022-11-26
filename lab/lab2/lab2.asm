e84beac7ff898a70aa4d0483923a446efc42a176
GeSongbo <948324533@qq.com>
2022-11-26 19:37:59 +0800

    something about F(N-2)%p

--- lab/lab2/lab2.asm
+++ lab/lab2/lab2.asm
@@ -2,16 +2,13 @@
 LD R0, x0FF
 LD R1, x0FF
 LD R2, x0FF
-NOT R3, R0
-ADD R3, R3, x1
+ADD R3, R0, #-1
 NOT R4, R1
 ADD R4, R4, x1
 ADD R5, R5, #1
 ADD R6, R6, #1
 ADD R2, R2, #-1
-ADD R5, R5, R3 
-BRzp #-2
-ADD R5, R5, R0
+AND R5, R5, R3 
 ADD R7, R6, #0
 ADD R7, R7, R4
 BRzp #-2
@@ -20,7 +17,7 @@ ADD R7, R7, R5
 ADD R5, R6, #0
 ADD R6, R7, #0
 ADD R2, R2, #-1
-BRp #-11
-ST R7, x0EC
+BRp #-10
+ST R7, x0EF
 TRAP x25
 .END
