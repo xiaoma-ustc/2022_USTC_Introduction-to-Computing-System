53cee71507b98637fa3dd448fba4d924c459ee04
GeSongbo <948324533@qq.com>
2022-11-29 10:14:19 +0800

    lab3

---
+++ lab/lab3/lab3.asm
@@ -0,0 +1,40 @@
+.ORIG x3000
+INIT LDI R0, NUM
+    LD R1, DATA
+    LDR R2, R1, #0
+    ADD R3, R2, #0
+    ADD R1, R1, #1
+    ADD R5, R5, #1
+    ADD R4, R4, #1
+    ADD R0, R0, #-1
+WHILE BRz UPDATE
+    LDR R2, R1, #0
+    ADD R1, R1, #1
+    NOT R6, R3
+    AND R6, R6, R2
+    BRnp UPDATE
+    ADD R4, R4, #1
+BACK ADD R3, R2, #0
+    ADD R0, R0, #-1
+    BRnzp WHILE
+    
+UPDATE NOT R6, R4
+    ADD R6, R5, R6
+    BRzp #1
+    ADD R5, R4, #0
+    AND R4, R4, #0
+    ADD R4, R4, #1
+    ADD R0, R0, #0
+    BRz OUTPUT
+    BRnzp BACK
+
+
+OUTPUT STI R5, RESULT
+
+HALT
+
+RESULT .FILL x3050
+NUM .FILL x3100
+DATA .FILL x3101
+.END
+
